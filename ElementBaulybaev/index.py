875d945cbcbf4a87eb72285ba2e2fc3f3f30b84e
Pomidor12 <73022500+Pomidor12@users.noreply.github.com>
2024-12-17 23:52:59 +0500

    Create index.py

---
+++ ElementBaulybaev/index.py
@@ -0,0 +1,27 @@
+class ElementBaulybaev():
+
+    def __init__(self, name, symbol, number):
+        self._name = name
+        self._symbol = symbol
+        self._number = number
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def symbol(self):
+        return self._symbol
+    
+    @property
+    def number(self):
+        return self._number
+
+    def dump(self):
+        print(f'name: {self.name} \nsymbol: {self.symbol} \nnumber: {self.number}')
+        
+
+object = ElementBaulybaev('Dinar', 'Li', 3)
+object.dump()
+
+print(object.name)
